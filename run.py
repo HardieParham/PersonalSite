c2b60ef4d0332a88e8f4e915cfefebc211299849
crazymugg <crazymugg.gaming@gmail.com>
2023-05-09 22:59:26 +0200

    run.py changes 3

--- run.py
+++ run.py
@@ -1,24 +1,24 @@
-# from app.app import create_app
-from flask import Flask, render_template
-
-# if __name__ == "__main__":
-#     app = create_app()  
-#     app.run(debug=False)
+# from flask import Flask, render_template
+# from app.views import views
 
 
+# my_app = Flask(__name__, static_folder='app/static', template_folder='app/templates')
+# my_app.register_blueprint(views)
 
-from app.views import views
-
-
-my_app = Flask(__name__, static_folder='app/static', template_folder='app/templates')
-my_app.register_blueprint(views)
-
-@my_app.route('/')
-def base():
-    return render_template('base.html')
+# @my_app.route('/')
+# def base():
+#     return render_template('base.html')
     
+# if __name__ == "__main__":
+#     my_app.run(debug=False)
+
+
+
+from app.app import create_app
+app = create_app()  
 if __name__ == "__main__":
-    my_app.run(debug=False)
+    app.run(debug=False)
+
 
 
 
