6b2027222f2282c3b2990bc3108b3a73ce909697
crazymugg <crazymugg.gaming@gmail.com>
2023-06-06 16:02:59 +0200

    Contact page work

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import datetime
 
 #External Imports
 from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 
 #Local Imports
 from src.views import views
@@ -13,11 +14,19 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
     app.register_blueprint(views)
+    mail = Mail(app)
 
 
     @app.route('/')
     def base():
         return render_template('base.html')
 
-    
+    @app.route('/process_email', methods=['POST'])
+    def process_email():
+        msg = Message('Test', sender='testaccount@gmail.com', recipients=['your@email.com'])
+        msg.body = 'This is a test email' #Customize based on user input
+        mail.send(msg)
+        return 'done'
+
+
     return app
--- src/config.py
+++ src/config.py
@@ -1,5 +1,7 @@
 class config:
-    MAIL_SERVER=''
-    MAIL_PORT=''
-    MAIL_USERNAME=''
-    MAIL_PASSWORD=''
+    MAIL_SERVER = 'smtp.googlemail.com',
+    MAIL_PORT = 465,
+    MAIL_USE_TLS = False,
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = 'testaccount',
+    MAIL_PASSWORD = '[password]'
