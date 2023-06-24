354fcff41e8247d359e15aa0c0040052816838e0
crazymugg <78958134+crazymugg@users.noreply.github.com>
2023-06-24 23:00:50 +0200

    source.py cleanup

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import datetime
 
 #External Imports
 from flask import Flask, render_template, request
-from flask_mail import Mail, Message
+# from flask_mail import Mail, Message
 
 #Local Imports
 from src.views import views
@@ -14,22 +14,22 @@ def create_app():
     app = Flask(__name__, template_folder='dist')
     app.config.from_object(config)
     app.register_blueprint(views)
-    mail = Mail(app)
+    # mail = Mail(app)
 
     @app.route('/')
     def three():
-        return render_template('index.html')
+        return render_template('home.html')
 
     @app.route('/old')
     def old():
         return render_template('base.html')
 
-    @app.route('/process_email', methods=['POST'])
-    def process_email():
-        msg = Message('Test', sender='testaccount@gmail.com', recipients=['your@email.com'])
-        msg.body = 'This is a test email' #Customize based on user input
-        mail.send(msg)
-        return 'done'
+    # @app.route('/process_email', methods=['POST'])
+    # def process_email():
+    #     msg = Message('Test', sender='testaccount@gmail.com', recipients=['your@email.com'])
+    #     msg.body = 'This is a test email' #Customize based on user input
+    #     mail.send(msg)
+    #     return 'done'
 
 
     return app
