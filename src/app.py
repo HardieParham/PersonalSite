9cb7cdcb5f0de547d5f9c67a5ad311de0c015791
Hardie Parham <78958134+crazymugg@users.noreply.github.com>
2024-02-04 21:03:17 -0600

    Python code cleanup

--- src/app.py
+++ src/app.py
@@ -1,20 +1,11 @@
-#Standard imports
-import datetime
-
-#External Imports
 from flask import Flask, render_template, request
-# from flask_mail import Mail, Message
 
-#Local Imports
 from src.views import views
-from src.config import config
 
 
 def create_app():
     app = Flask(__name__, template_folder='dist')
-    app.config.from_object(config)
     app.register_blueprint(views)
-    # mail = Mail(app)
 
     @app.route('/commands')
     def commands():
@@ -23,13 +14,5 @@ def create_app():
     @app.route('/')
     def old():
         return render_template('base.html')
-
-    # @app.route('/process_email', methods=['POST'])
-    # def process_email():
-    #     msg = Message('Test', sender='testaccount@gmail.com', recipients=['your@email.com'])
-    #     msg.body = 'This is a test email' #Customize based on user input
-    #     mail.send(msg)
-    #     return 'done'
-
-
+    
     return app
--- src/config.py
+++
@@ -1,7 +0,0 @@
-class config:
-    MAIL_SERVER = 'smtp.googlemail.com',
-    MAIL_PORT = 465,
-    MAIL_USE_TLS = False,
-    MAIL_USE_SSL = True,
-    MAIL_USERNAME = 'testaccount',
-    MAIL_PASSWORD = '[password]'
