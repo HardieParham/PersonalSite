f44dac720e46067f1499a0ba2ee3b8a122b09a99
crazymugg <crazymugg.gaming@gmail.com>
2024-08-29 16:17:54 -0500

    added logging statements

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from src.app import create_app
 my_app = create_app()
   
 if __name__ == "__main__":
+    print("Running app.")
     my_app.run(debug=False, host='0.0.0.0', port=5000)
--- src/app.py
+++ src/app.py
@@ -15,24 +15,28 @@ def create_app():
     # Main route used as the standard container for JQuery to append to
     @app.route('/')
     def main():
+        print("Rendering Base Page.")
         return render_template('base.html')
     
     # Seconday route to show my personal resume in the browser
     @app.route('/resume')
     def view_resume():
         path = "files/Hardie Parham Resume 2024 V3.pdf"
+        print("Viewing Resume.")
         return send_from_directory('static', path)
     
     # Seconday route to download my personal resume as an attachment
     @app.route('/sendresume')
     def send_resume():
         path = "static/files/Hardie Parham Resume 2024 V3.pdf"
+        print("Sending Resume File.")
         return send_file(path, as_attachment=True)
     
     # Route for "Page not found"
     @app.errorhandler(404) 
-    def not_found(e): 
+    def not_found(e):
+        print("Rendering 404 Page.")
         return render_template("404.html") 
 
-
+    print("App created.")
     return app
