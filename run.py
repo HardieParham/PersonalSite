7653a279076da19bb7b66b8a8bd4692406e98ef3
Hardie Parham <hparham865@gmail.com>
2025-01-28 08:57:49 -0600

    variables update

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from src.app import create_app
 my_app = create_app()
   
 if __name__ == "__main__":
-    my_app.run(debug=False, host='0.0.0.0', port=5000)
+    my_app.run(debug=True, host='0.0.0.0', port=5000)
