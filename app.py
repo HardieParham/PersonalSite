2ddfb1610122f79a2c9cd5660e0e955c945b0476
Hardie Parham <78958134+HardieParham@users.noreply.github.com>
2025-05-21 14:46:30 -0500

    renamed run to app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from src.app import create_app
 
-my_app = create_app()
+app = create_app()
   
 if __name__ == "__main__":
     print("Running app.")
-    my_app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
